3bde1b54c7ee7023d3eedb33731cb37fc4f43583
Lukas Lanzner <LanznerLukas@dotspace.dev>
2022-10-20 14:41:18 +0200

    initial commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+import processing.core.PApplet;
+
+public class Main extends PApplet {
+
+  public static void main(String[] args) {
+    PApplet.main(Main.class);
+  }
+
+  public void settings() {
+    size(800, 800);
+  }
+
+  public void setup() {
+    background(255);
+  }
+
+  public void draw() {
+  }
+}
